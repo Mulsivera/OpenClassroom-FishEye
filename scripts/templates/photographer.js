6490cf7691a4677e221325eebed4eb7399f73f19
mulsivera <adrien.dasilva.ferreira@gmail.com>
2025-10-03 14:27:09 +0200

    feat(photographer): aria-label + link added

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -12,6 +12,10 @@ class Photographer {
     const article = document.createElement('article');
     article.setAttribute("class", "photographer")
 
+    const link = document.createElement('a');
+    link.setAttribute("class", "photographer__link");
+    link.setAttribute("href", "#");
+
     const portrait = document.createElement('img');
     portrait.setAttribute("src", `assets/photographers/photographers_id_photos/${this.portrait}`)
     portrait.setAttribute("class", "photographer__portrait");
@@ -19,25 +23,30 @@ class Photographer {
 
     const name = document.createElement('h2');
     name.setAttribute("class", "photographer__name")
+    name.setAttribute("aria-label", "Nom du photographe : " + this.name);
     name.textContent = this.name;
 
     const localisation = document.createElement('p');
     localisation.setAttribute("class", "photographer__localisation");
+    localisation.setAttribute("aria-label", "localisé à " + this.city + ", " + this.country);
     localisation.textContent = this.city + ", " + this.country;
 
     const tagline = document.createElement('p');
     tagline.setAttribute("class", "photographer__tagline");
+    tagline.setAttribute("aria-label", "Slogan : " + this.tagline);
     tagline.textContent = this.tagline;
 
     const price = document.createElement('p');
     price.setAttribute("class", "photographer__price");
+    price.setAttribute("aria-label", "Prix : " + this.price + " euros par jour");
     price.textContent = this.price +"€/jour";
 
-    article.appendChild(portrait);
-    article.appendChild(name);
-    article.appendChild(localisation);
-    article.appendChild(tagline);
-    article.appendChild(price);
+    article.appendChild(link);
+    link.appendChild(portrait);
+    link.appendChild(name);
+    link.appendChild(localisation);
+    link.appendChild(tagline);
+    link.appendChild(price);
 
     return article
 
